34862f07823d0258f92ae2d2512eddc5c8d721be
lloyd <lloyd@randombit.net>
2009-02-11 03:43:57 +0000

    Remove DL_Scheme use from NR implementation

--- src/pubkey/nr/nr.cpp
+++ src/pubkey/nr/nr.cpp
@@ -1,15 +1,20 @@
 /*
 * Nyberg-Rueppel Source File
-* (C) 1999-2007 Jack Lloyd
+* (C) 1999-2009 Jack Lloyd
 */
 
 #include <botan/nr.h>
 #include <botan/numthry.h>
 #include <botan/keypair.h>
 #include <botan/look_pk.h>
+#include <botan/der_enc.h>
+#include <botan/ber_dec.h>
 
 namespace Botan {
 
+/**
+* Decode a NR public key from subjectPublicKeyInfo format
+*/
 NR_PublicKey::NR_PublicKey(const AlgorithmIdentifier& alg_id,
                            const MemoryRegion<byte>& key_bits)
    {
@@ -23,11 +28,35 @@ NR_PublicKey::NR_PublicKey(const AlgorithmIdentifier& alg_id,
 /**
 * NR_PublicKey Constructor
 */
-NR_PublicKey::NR_PublicKey(const DL_Group& grp, const BigInt& y1)
+NR_PublicKey::NR_PublicKey(const DL_Group& grp, const BigInt& pub_key)
    {
    group = grp;
-   y = y1;
+   y = pub_key;
    core = NR_Core(group, y);
+   }
+
+/**
+* Check NR public key for consistency
+*/
+bool NR_PublicKey::check_key(RandomNumberGenerator& rng,
+                             bool strong) const
+   {
+   if(y < 2 || y >= group_p())
+      return false;
+   if(!group.verify_group(rng, strong))
+      return false;
+   return true;
+   }
+
+std::pair<AlgorithmIdentifier, MemoryVector<byte> >
+NR_PublicKey::subject_public_key_info() const
+   {
+   AlgorithmIdentifier alg_id(this->get_oid(),
+                              this->group.DER_encode(DL_Group::ANSI_X9_57));
+
+   MemoryVector<byte> key_bits = DER_Encoder().encode(this->get_y()).get_contents();
+
+   return std::make_pair(alg_id, key_bits);
    }
 
 /**
@@ -36,22 +65,6 @@ NR_PublicKey::NR_PublicKey(const DL_Group& grp, const BigInt& y1)
 SecureVector<byte> NR_PublicKey::verify(const byte sig[], u32bit sig_len) const
    {
    return core.verify(sig, sig_len);
-   }
-
-/**
-* Return the maximum input size in bits
-*/
-u32bit NR_PublicKey::max_input_bits() const
-   {
-   return (group_q().bits() - 1);
-   }
-
-/**
-* Return the size of each portion of the sig
-*/
-u32bit NR_PublicKey::message_part_size() const
-   {
-   return group_q().bytes();
    }
 
 /**
@@ -112,26 +125,50 @@ SecureVector<byte> NR_PrivateKey::sign(const byte in[], u32bit length,
    }
 
 /**
+* @return encoding of NR private key for PKCS #8
+*/
+std::pair<AlgorithmIdentifier, SecureVector<byte> >
+NR_PrivateKey::pkcs8_encoding() const
+   {
+   AlgorithmIdentifier alg_id(this->get_oid(),
+                              this->group.DER_encode(DL_Group::ANSI_X9_57));
+
+   SecureVector<byte> key_bits =
+      DER_Encoder().encode(this->get_x()).get_contents();
+
+   return std::make_pair(alg_id, key_bits);
+   }
+
+/**
 * Check Private Nyberg-Rueppel Parameters
 */
 bool NR_PrivateKey::check_key(RandomNumberGenerator& rng, bool strong) const
    {
-   if(!DL_Scheme_PrivateKey::check_key(rng, strong) || x >= group_q())
+   const BigInt& p = group_p();
+   const BigInt& g = group_g();
+
+   if(y < 2 || y >= p || x < 2 || x >= p)
       return false;
 
-   if(!strong)
-      return true;
-
-   try
-      {
-      KeyPair::check_key(rng,
-                         get_pk_signer(*this, "EMSA1(SHA-1)"),
-                         get_pk_verifier(*this, "EMSA1(SHA-1)")
-         );
-      }
-   catch(Self_Test_Failure)
-      {
+   if(!group.verify_group(rng, strong))
       return false;
+
+   if(strong)
+      {
+      if(y != power_mod(g, x, p))
+         return false;
+
+      try
+         {
+         KeyPair::check_key(rng,
+                            get_pk_signer(*this, "EMSA1(SHA-1)"),
+                            get_pk_verifier(*this, "EMSA1(SHA-1)")
+            );
+         }
+      catch(Self_Test_Failure)
+         {
+         return false;
+         }
       }
 
    return true;
--- src/pubkey/nr/nr.h
+++ src/pubkey/nr/nr.h
@@ -1,61 +1,116 @@
 /*
 * Nyberg-Rueppel Header File
-* (C) 1999-2007 Jack Lloyd
+* (C) 1999-2009 Jack Lloyd
 */
 
 #ifndef BOTAN_NYBERG_RUEPPEL_H__
 #define BOTAN_NYBERG_RUEPPEL_H__
 
-#include <botan/dl_algo.h>
+#include <botan/pk_keys.h>
+#include <botan/dl_group.h>
 #include <botan/nr_core.h>
+#include <botan/rng.h>
 
 namespace Botan {
 
 /**
 * Nyberg-Rueppel Public Key
 */
-class BOTAN_DLL NR_PublicKey : public PK_Verifying_with_MR_Key,
-                               public virtual DL_Scheme_PublicKey
+class BOTAN_DLL NR_PublicKey : public PK_Verifying_with_MR_Key
    {
    public:
-      std::string algo_name() const { return "NR"; }
-
-      SecureVector<byte> verify(const byte[], u32bit) const;
-      u32bit max_input_bits() const;
-
-      DL_Group::Format group_format() const { return DL_Group::ANSI_X9_57; }
-      u32bit message_parts() const { return 2; }
-      u32bit message_part_size() const;
-
-      NR_PublicKey() {}
+      NR_PublicKey(const DL_Group& group, const BigInt& y);
 
       NR_PublicKey(const AlgorithmIdentifier& alg_id,
                    const MemoryRegion<byte>& key_bits);
 
-      NR_PublicKey(const DL_Group&, const BigInt&);
+      std::string algo_name() const { return "NR"; }
+
+      SecureVector<byte> verify(const byte[], u32bit) const;
+
+      /**
+      * Get the DL domain parameters of this key.
+      * @return the DL domain parameters of this key
+      */
+      const DL_Group& get_domain() const { return group; }
+
+      /**
+      * Get the public value y with y = g^x mod p where x is the secret key.
+      */
+      const BigInt& get_y() const { return y; }
+
+      /**
+      * Get the prime p of the underlying DL group.
+      * @return the prime p
+      */
+      const BigInt& group_p() const { return group.get_p(); }
+
+      /**
+      * Get the prime q of the underlying DL group.
+      * @return the prime q
+      */
+      const BigInt& group_q() const { return group.get_q(); }
+
+      /**
+      * Get the generator g of the underlying DL group.
+      * @return the generator g
+      */
+      const BigInt& group_g() const { return group.get_g(); }
+
+      bool check_key(RandomNumberGenerator& rng, bool) const;
+
+      u32bit message_parts() const { return 2; }
+
+      /**
+      * Return the size of each portion of the sig
+      */
+      u32bit message_part_size() const { return group_q().bytes(); }
+
+      /**
+      * Return the maximum input size in bits
+      */
+      u32bit max_input_bits() const { return (group_q().bits() - 1); }
+
+      std::pair<AlgorithmIdentifier, MemoryVector<byte> >
+         subject_public_key_info() const;
+
    protected:
+      NR_PublicKey() {}
+
       NR_Core core;
+      DL_Group group;
+      BigInt y;
    };
 
 /**
 * Nyberg-Rueppel Private Key
 */
 class BOTAN_DLL NR_PrivateKey : public NR_PublicKey,
-                                public PK_Signing_Key,
-                                public virtual DL_Scheme_PrivateKey
+                                public PK_Signing_Key
    {
    public:
-      SecureVector<byte> sign(const byte[], u32bit,
+      SecureVector<byte> sign(const byte msg[], u32bit msg_len,
                               RandomNumberGenerator& rng) const;
-
-      bool check_key(RandomNumberGenerator& rng, bool) const;
 
       NR_PrivateKey(const AlgorithmIdentifier& alg_id,
                     const MemoryRegion<byte>& key_bits,
                     RandomNumberGenerator& rng);
 
-      NR_PrivateKey(RandomNumberGenerator&, const DL_Group&,
-                    const BigInt& = 0);
+      NR_PrivateKey(RandomNumberGenerator& rng, const DL_Group& group,
+                    const BigInt& x = 0);
+
+      bool check_key(RandomNumberGenerator& rng, bool strong) const;
+
+      /**
+      * Get the secret key x.
+      * @return the secret key
+      */
+      const BigInt& get_x() const { return x; }
+
+      std::pair<AlgorithmIdentifier, SecureVector<byte> >
+         pkcs8_encoding() const;
+   private:
+      BigInt x;
    };
 
 }
