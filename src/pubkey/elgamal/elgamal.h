3859fcb2611f37376f7cee42d87bbf2376bbdc83
lloyd <lloyd@randombit.net>
2009-02-11 03:47:06 +0000

    Reorg ElGamal header, add Doxygen coments

--- src/pubkey/elgamal/elgamal.h
+++ src/pubkey/elgamal/elgamal.h
@@ -18,20 +18,21 @@ class BOTAN_DLL ElGamal_PublicKey : public PK_Encrypting_Key,
                                     public virtual DL_Scheme_PublicKey
    {
    public:
-      std::string algo_name() const { return "ElGamal"; }
-      DL_Group::Format group_format() const { return DL_Group::ANSI_X9_42; }
-
-      SecureVector<byte> encrypt(const byte[], u32bit,
-                                 RandomNumberGenerator& rng) const;
-      u32bit max_input_bits() const;
-
-      ElGamal_PublicKey() {}
-
       ElGamal_PublicKey(const AlgorithmIdentifier& alg_id,
                         const MemoryRegion<byte>& key_bits);
 
-      ElGamal_PublicKey(const DL_Group&, const BigInt&);
+      ElGamal_PublicKey(const DL_Group& group, const BigInt& y);
+
+      std::string algo_name() const { return "ElGamal"; }
+      DL_Group::Format group_format() const { return DL_Group::ANSI_X9_42; }
+
+      SecureVector<byte> encrypt(const byte msg[], u32bit msg_len,
+                                 RandomNumberGenerator& rng) const;
+
+      u32bit max_input_bits() const;
    protected:
+      ElGamal_PublicKey() {}
+
       ELG_Core core;
    };
 
@@ -43,16 +44,16 @@ class BOTAN_DLL ElGamal_PrivateKey : public ElGamal_PublicKey,
                                      public virtual DL_Scheme_PrivateKey
    {
    public:
-      SecureVector<byte> decrypt(const byte[], u32bit) const;
-
-      bool check_key(RandomNumberGenerator& rng, bool) const;
+      ElGamal_PrivateKey(RandomNumberGenerator& rng, const DL_Group& group,
+                         const BigInt& x = 0);
 
       ElGamal_PrivateKey(const AlgorithmIdentifier& alg_id,
                          const MemoryRegion<byte>& key_bits,
                          RandomNumberGenerator& rng);
 
-      ElGamal_PrivateKey(RandomNumberGenerator&, const DL_Group&,
-                         const BigInt& = 0);
+      SecureVector<byte> decrypt(const byte msg[], u32bit msg_len) const;
+
+      bool check_key(RandomNumberGenerator& rng, bool) const;
    };
 
 }
