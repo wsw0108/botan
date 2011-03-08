b0ad2b2465a77610f69497657aa10e72280b3a3f
lloyd <lloyd@randombit.net>
2011-03-08 22:25:00 +0000

    Possible new syntax

--- doc/examples/make_pipe.cpp
+++ doc/examples/make_pipe.cpp
@@ -9,24 +9,10 @@ int main()
    {
    LibraryInitializer init;
 
-   Pipe* p1 = make_pipe("");
-
-   std::cout << p1 << "\n";
-   delete p1;
-
-   Pipe* p2 = make_pipe("encode:hex");
-   std::cout << p2 << "\n";
-   delete p2;
-
-   /*
-   Pipe* p3 = make_pipe("decode:hex");
-
-   Pipe p4 = make_pipe("encode:hex | decode:hex");
-
-   Pipe* p5 = make_pipe("compress:zlib(9) |"
-                        "cipher:cbc:aes-192(aes_key, aes_iv) |"
-                        "(passthrough,"
-                        "(hmac-sha1(hmac_key) | encode:base64))");
-   */
+   Pipe* p5 = make_pipe(
+     "compress:zlib(9), "
+     "cipher:cbc:aes-192(aes_key, aes_iv), "
+     "fork(passthrough, chain(mac:hmac-sha1(hmac_key), encode:base64))"
+     );
 
    }
